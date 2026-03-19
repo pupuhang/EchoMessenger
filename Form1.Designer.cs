074e710033fa0a94697a5ede49779dad4c29b24c
지웅 최 <sz2558@naver.com>
2026-03-19 10:45:19 +0900

    과제1 코딩과 테스트 완료 ListBox를 중앙에 배치하고 프로그램 이름을 ListBox 위에 배치 TextBox와 Button 아래에 배치 TextBox에 키보드로 값을 입력하고 전송 Button을 누르면 ListBox에 값을 출력하고 TextBox 내용을 제거하고 다음 입력을 기다

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            listMessage = new ListBox();
+            lblName = new Label();
+            txtMessage = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // listMessage
+            // 
+            listMessage.FormattingEnabled = true;
+            listMessage.Location = new Point(12, 74);
+            listMessage.Name = "listMessage";
+            listMessage.Size = new Size(776, 304);
+            listMessage.TabIndex = 0;
+            listMessage.SelectedIndexChanged += listMessage_SelectedIndexChanged;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.BackColor = Color.Transparent;
+            lblName.Font = new Font("맑은 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblName.ForeColor = SystemColors.Highlight;
+            lblName.Location = new Point(12, 22);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(251, 41);
+            lblName.TabIndex = 1;
+            lblName.Text = "Echo Messenger";
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(12, 398);
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(607, 27);
+            txtMessage.TabIndex = 2;
+            txtMessage.TextChanged += txtMessage_TextChanged;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.GreenYellow;
+            btnSend.Location = new Point(625, 384);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(163, 54);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtMessage);
+            Controls.Add(lblName);
+            Controls.Add(listMessage);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listMessage;
+        private Label lblName;
+        private TextBox txtMessage;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Net.Mime.MediaTypeNames;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -5,6 +7,24 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listMessage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //Text를 ListBox에 추가
+            listMessage.Items.Add(txtMessage.Text);
+            //TextBox 초기화
+            txtMessage.Clear();
         }
     }
 }
